d82a0497a50e1c451e390e95721fc0da373073ae
Hashir Sajid <hs96300k@gmail.com>
2025-05-05 02:29:37 +0500

    updated

--- portfolio/src/components/Home/Home.jsx
+++ portfolio/src/components/Home/Home.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import TrueFocus from './TrueFocus';
+import { FaLinkedin, FaGithub, FaWhatsapp } from 'react-icons/fa';
+import ShinyText from './ShinyText';
+import './Home.css';
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const sentences = [
+    "Transforming Challenges into Opportunities",
+    "Bringing Ideas to Life Through Code",
+    "A Full Stack Web Developer",
+    "Transforming Ideas into Code"
+  ];
 
-export default Home
+  const [currentSentenceIndex, setCurrentSentenceIndex] = useState(0);
+
+  const handleAnimationComplete = () => {
+    setTimeout(() => {
+      setCurrentSentenceIndex((prev) => (prev + 1) % sentences.length);
+    }, 1000); // Delay before showing the next sentence
+  };
+
+  return (
+    <div className="home-container">
+      <h1>Hi, I am Hashir Sajid</h1>
+      <div className="focus-text">
+        <TrueFocus
+          sentence={sentences[currentSentenceIndex]}
+          manualMode={false}
+          blurAmount={5}
+          borderColor="red"
+          animationDuration={2}
+          pauseBetweenAnimations={1}
+          onComplete={handleAnimationComplete}
+        />
+      </div>
+      <p className="home-paragraph">
+        Versatile and reliable web developer skilled in tackling challenges, crafting clean, production-ready code, and thriving in dynamic, high-pressure environments. I bring hands-on experience with React.js for front-end development and expertise in Node.js, Express.js, and MongoDB for back-end solutions.
+      </p>
+      <div className="social-icons">
+        <a href="https://www.linkedin.com/in/hashirsajid/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin size={30} color="#0077B5" />
+        </a>
+        <a href="https://github.com/hs96300k" target="_blank" rel="noopener noreferrer">
+          <FaGithub size={30} color="#ffffff" />
+        </a>
+        <a href="https://api.whatsapp.com/send/?phone=923704353050&text&type=phone_number&app_absent=0" target="_blank" rel="noopener noreferrer">
+          <FaWhatsapp size={30} color="#25D366" />
+        </a>
+      </div>
+      <a href="/cv.pdf" download>
+        <ShinyText text="Download CV" speed={3} className="cv-button" />
+      </a>
+    </div>
+  );
+};
+
+export default Home;
